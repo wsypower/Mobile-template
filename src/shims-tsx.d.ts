d1bc89a7da0a3f2677cdabab1979960e14915b44
weiyafei <2553241022@qq.com>
2019-10-22 15:38:12 +0800

    注释shims-**文件

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,5 +1,18 @@
+/*
+ * @Author: wei.yafei
+ * @Date: 2019-10-22 15:32:59
+ * @Last Modified by: wei.yafei 
+ * @Last Modified time: 2019-10-22 15:35:35
+ */
 import Vue, { VNode } from "vue";
 
+/**
+ * @description 允许你以.tsx结尾的文件，在Vue项目中编写jsx代码
+ * @author vue官方
+ * @date 2019-10-22
+ * @interface Element
+ * @extends {VNode}
+ */
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,15 @@
+/*
+ * @Author: wei.yafei
+ * @Date: 2019-10-22 15:36:18
+ * @Last Modified by: wei.yafei
+ * @Last Modified time: 2019-10-22 15:37:20
+ */
+
+/**
+ * @description 主要用于 TypeScript 识别.vue 文件
+ * @author vue官方
+ * @date 2019-10-22 15:36:20
+ */
 declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
