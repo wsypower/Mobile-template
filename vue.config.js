120b4d2f33c1e8a26a82af8f0ca05514b4aa6a95
weiyafei <2553241022@qq.com>
2019-11-04 15:21:40 +0800

    feat: 🎸 脚手架只开发完成webpack部分配置,后续需要在跟进配置项目
    目前开发完成功能:	1.scss 2.vue.config,js dll.js 3.git 4.fastclick 5.responsive

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: wei.yafei
  * @Date: 2019-10-22 16:32:01
  * @Last Modified by: wei.yafei 
- * @Last Modified time: 2019-11-01 14:09:08
+ * @Last Modified time: 2019-11-01 15:20:06
  */
 
 /**
